51f96c779ce9513bb4ed86d71cdd4b371cd03967
Labib <cse.siddiqul@gmail.com>
2024-03-20 12:47:39 +0600

    auth seleice

--- src/components/redux/features/authSlice.js
+++ src/components/redux/features/authSlice.js
@@ -1,10 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import axios from "axios";
 
-<<<<<<< HEAD
-
-=======
->>>>>>> apurbo
 export const loginUser = createAsyncThunk(
     "auth/loginUser",
     async (loginData, { rejectWithValue, fulfillWithValue }) => {
@@ -20,59 +16,21 @@ export const loginUser = createAsyncThunk(
     }
 );
 
-
-<<<<<<< HEAD
-
-const authSlice = createSlice({
-    name: "user",
-    initialState: {
-        loading: false,
-        isLoggedIn: false,
-=======
-// export const regUser = createAsyncThunk(
-//     "auth/regUser",
-//     async (regData, { rejectWithValue, fulfillWithValue }) => {
-//         try {
-//             // Perform the POST request to the login endpoint
-//             const response = await axios.post("http://127.0.0.1:8000/api/login", regData);
-//             // console.log("lllllllllllllllllll", response)
-//             // Return the response data upon successful login
-//             return fulfillWithValue(response.data);
-//         } catch (error) {
-//             // Return the error data upon encountering an error
-//             // You might want to handle different types of errors more explicitly here
-//             return rejectWithValue(error.response.data);
-//         }
-//     }
-// );
-
-
-
-
-
-
-
-
-
 const authSlice = createSlice({
     name: "auth",
     initialState: {
         loading: false,
->>>>>>> apurbo
         token: "",
         user: "",
         message: "",
         error: null,
     },
-<<<<<<< HEAD
-=======
     reducers: {
         logout() {
             localStorage.removeItem("token");
 
         }
     },
->>>>>>> apurbo
     extraReducers: (builder) => {
 
         builder.addCase(loginUser.pending, (state) => {
@@ -83,10 +41,7 @@ const authSlice = createSlice({
                 state.loading = false;
                 state.user = action.payload.user;
                 state.token = action.payload.token;
-<<<<<<< HEAD
                 state.isLoggedIn = true;
-=======
->>>>>>> apurbo
                 state.message = action.payload.message || "success";
                 localStorage.setItem("token", action.payload.token);
             })
@@ -97,16 +52,6 @@ const authSlice = createSlice({
 
     }
 
-
 });
 
-
-
-<<<<<<< HEAD
-
-
-
-
-=======
->>>>>>> apurbo
 export default authSlice.reducer;
